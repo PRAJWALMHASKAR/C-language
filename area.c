fccc9226a25e864538bfb3eed83d65f11d2d9a9d
PRAJWALMHASKAR <33192878+PRAJWALMHASKAR@users.noreply.github.com>
2017-11-10 12:30:21 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,19 @@
+/* program FOR AREA OF CIRCLE*/
+#include<stdio.h>
+void main()
+{
+int r=13;
+const float pi=3.14;
+float a;
+a=pi*r*r;
+printf("%f",a);
+}
+
+
+/*
+********************************  output  **************************************
+dell@dell-OptiPlex-7040:~$ gcc area.c
+dell@dell-OptiPlex-7040:~$ ./a.out
+530.659973
+*********************************  output  *************************************
+*/
